4f6df5f3ab8802c027948cde95cd837f3f470e71
amoscicki <arkadiuszmoscicki@gmail.com>
2024-05-20 13:33:24 +0100

    initial commit

---
+++ app/src/main/java/com/example/prm2/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.prm2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.prm2.ui.theme.PRM2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PRM2Theme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {TopMenu()},
+                    ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopMenu() {
+
+        TopAppBar(
+        modifier = Modifier,
+        title = { Text(text = stringResource(R.string.app_title)) },
+        navigationIcon = {
+            IconButton(
+                onClick = { /* doSomething() */ }
+            ) {
+                Icon(Icons.Filled.DateRange, contentDescription = null)
+            }
+        },
+        actions = {
+            // RowScope here, so these icons will be placed horizontally
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.LocationOn, contentDescription = null)
+            }
+        }
+    )
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PRM2Theme {
+        Greeting("Android")
+    }
+}
