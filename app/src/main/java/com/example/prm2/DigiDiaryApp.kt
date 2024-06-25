7303756b39125a545da875362b79501889732a00
amoscicki <arkadiuszmoscicki@gmail.com>
2024-06-26 00:23:49 +0100

    costam

--- app/src/main/java/com/example/prm2/DigiDiaryApp.kt
+++ app/src/main/java/com/example/prm2/DigiDiaryApp.kt
@@ -1,16 +1,28 @@
 package com.example.prm2
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import com.example.prm2.dependencyinjection.DigiDiaryModule
 
 class DigiDiaryApp : Application() {
 
     companion object {
+        @SuppressLint("StaticFieldLeak")
         lateinit var instance: DigiDiaryModule
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = DigiDiaryModule(this)
+        val channel = NotificationChannel(
+            "location",
+            "DigiDiaryLocationService",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
